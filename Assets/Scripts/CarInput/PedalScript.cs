2aa44a5723c98757b14d5689976d99aa6ab9069b
David <dekuffzain@gmail.com>
2026-05-31 14:52:49 +0300

    Implemented pedal behaviour for GAS and BREAKS

--- Assets/Scripts/CarInput/PedalScript.cs
+++ Assets/Scripts/CarInput/PedalScript.cs
@@ -3,13 +3,53 @@ using UnityEngine.InputSystem;
 
 public class PedalScript : MonoBehaviour
 {
+    [Header("Pedal Settings")]
+    [SerializeField] private float ConstMinPressure = 0f;
+    [SerializeField] private float InputPressureRate = 0.1f;
+    [SerializeField] private float MaxInputPedalPressure = 1f; // Note. maximum pressure a PLAYER can inflict on the pedal using input
+    
+    [SerializeField] private float PressureDampeningRate = 0.1f;
+    [SerializeField] private float PressureDampeningInterval = 0.5f; // Note. In seconds
+
+
     [Header("Player Input")]
     [SerializeField] private InputAction playerPress;
+
+    // Class Private Fields
+    private float DampenTimeElapsed = 0f;
+
+    // Class Properties
+    internal float MaxPedalPressure { get; private set; }
+    internal float InputPedalPressure { get; private set; }
+    internal float PedalPressure => MaxPedalPressure * (InputPedalPressure + ConstMinPressure);
 
     private void OnEnable()
     {
         playerPress.Enable();
     }
+
+    private void Start()
+    {
+        MaxPedalPressure = Mathf.Clamp01(ConstMinPressure + MaxInputPedalPressure);
+        if (MaxPedalPressure < 1f)
+        {
+            Debug.LogWarning("Incorrect Pressure Assignment! ( MaxPedalPressure = ConstMinPressure + MaxPlayerPedalPressure != 1 ), this can cause unexpected behaviour");
+        }
+    }
+
+    private void Update()
+    {
+        if (playerPress.IsPressed())
+        {
+            ApplyPressure();
+            if (DampenTimeElapsed != 0) DampenTimeElapsed = 0;
+        }
+        else
+        {
+            AttemptPressureDampening();
+        }
+    }
+
     private void OnDisable()
     {
         playerPress.Disable();
@@ -19,4 +59,27 @@ public class PedalScript : MonoBehaviour
     {
         playerPress.Dispose();
     }
+
+    private void AttemptPressureDampening()
+    {
+        if (DampenTimeElapsed >= PressureDampeningInterval)
+        {
+            DampenPressure();
+            DampenTimeElapsed = 0;
+        }
+        else
+        {
+            DampenTimeElapsed += Time.deltaTime;
+        }
+    }
+
+    private void ApplyPressure() => InputPedalPressure = Mathf.Min(
+            InputPedalPressure += InputPressureRate * Time.deltaTime, 
+            MaxInputPedalPressure
+        );
+
+    private void DampenPressure() => InputPedalPressure = Mathf.Max(
+            InputPedalPressure - PressureDampeningRate, 
+            0
+        );
 }
